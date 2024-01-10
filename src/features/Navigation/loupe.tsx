cca610410e6aa7a0e9b4045ee3a2f69518f758db
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-01-10 23:09:32 +0100

    changed loupe svg

--- src/features/Navigation/loupe.tsx
+++ src/features/Navigation/loupe.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
 import { SVGProps } from "react";
 const Loupe = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24px" fill="none" {...props}>
-    <g stroke="#7E839A" strokeLinecap="round" strokeLinejoin="round">
-      <circle cx={11.766} cy={11.767} r={8.989} />
-      <path d="M18.018 18.485 21.542 22" />
-    </g>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
+    fill="#7e839a"
+    viewBox="0 0 512 512"
+    {...props}
+  >
+    <path d="m508.255 490.146-128-128c-.06-.06-.137-.077-.196-.128 34.193-38.434 55.142-88.917 55.142-144.418 0-120.175-97.425-217.6-217.6-217.6S.001 97.425.001 217.6s97.425 217.6 217.6 217.6c55.501 0 105.975-20.949 144.418-55.151.06.06.077.137.128.196l128 128c2.5 2.509 5.777 3.755 9.054 3.755s6.554-1.246 9.054-3.746c4.992-5.001 4.992-13.107 0-18.108zM217.601 409.6c-105.865 0-192-86.135-192-192s86.135-192 192-192 192 86.135 192 192-86.135 192-192 192z" />
   </svg>
 );
 export default Loupe;

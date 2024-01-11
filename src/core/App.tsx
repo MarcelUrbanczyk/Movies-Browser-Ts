f5088febdd299e4198f00ea4fc8fbfa3bbfb2eb0
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-01-11 22:06:26 +0100

    added movie list to app

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import Navigation from "../features/Navigation";
 import Main from "../common/main";
 import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import MovieList from "../features/List/Movies";
 
 const App = () => (
   <HashRouter>
     <Navigation />
     <Main>
       <Switch>
-        <Route path="/movies"></Route>
+        <Route path="/movies">
+          <MovieList />
+        </Route>
         <Route path="/shows"></Route>
         <Route path="/people"></Route>
         <Route path="/">

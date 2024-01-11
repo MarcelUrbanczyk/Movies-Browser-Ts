c07af0769630c387b50f07b24cf8878489d026f0
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-01-11 21:22:45 +0100

    implemented react router

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Navigation from "../features/Navigation";
 import Main from "../common/main";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 
 const App = () => (
-  <>
+  <HashRouter>
     <Navigation />
-    <Main></Main>
-  </>
+    <Main>
+      <Switch>
+        <Route path="/movies"></Route>
+        <Route path="/shows"></Route>
+        <Route path="/people"></Route>
+        <Route path="/">
+          <Redirect to="/movies" />
+        </Route>
+      </Switch>
+    </Main>
+  </HashRouter>
 );
 
 export default App;

faed576b0485113edb72a54e17911a1342708fac
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-01-11 21:01:21 +0100

    created theme

---
+++ src/common/theme.ts
@@ -0,0 +1,17 @@
+const colors = {
+  Woodsmoke: "#18181B",
+  White: "#FFFFFF",
+  Waterloo: "#7E839A",
+  Whisper: "#F5F5FA",
+};
+
+const theme = {
+  background: colors.Whisper,
+  box: colors.White,
+  textPrimary: colors.Woodsmoke,
+  textSecondary: colors.Waterloo,
+  navigation: {
+    background: colors.Woodsmoke,
+    text: colors.Waterloo,
+  },
+};

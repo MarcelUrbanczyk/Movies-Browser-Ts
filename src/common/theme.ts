eba4869fb3e1687d9648c38830733f97de7b189e
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-01-11 21:06:15 +0100

    added more values to theme

--- src/common/theme.ts
+++ src/common/theme.ts
@@ -3,13 +3,20 @@ const colors = {
   White: "#FFFFFF",
   Waterloo: "#7E839A",
   Whisper: "#F5F5FA",
+  Mystic: "#E4E6F0",
+  PattensBlue: "#D6E4FF",
 };
 
 export const theme = {
   background: colors.Whisper,
   box: colors.White,
+  alternateBox: colors.Mystic,
   textPrimary: colors.Woodsmoke,
   textSecondary: colors.Waterloo,
+  pagination: {
+    previous: colors.Mystic,
+    next: colors.PattensBlue,
+  },
   navigation: {
     background: colors.Woodsmoke,
     text: colors.Waterloo,

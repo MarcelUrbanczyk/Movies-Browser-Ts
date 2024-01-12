bd10cde9952286494affad6a81aa5b4e8975a809
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-01-12 21:09:06 +0100

    moved main to specific component and added pagination to movie list

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,24 +1,21 @@
 import React from "react";
 import Navigation from "../features/Navigation";
-import Main from "../common/main";
 import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import MovieList from "../features/List/Movies";
 
 const App = () => (
   <HashRouter>
     <Navigation />
-    <Main>
-      <Switch>
-        <Route path="/movies">
-          <MovieList />
-        </Route>
-        <Route path="/shows"></Route>
-        <Route path="/people"></Route>
-        <Route path="/">
-          <Redirect to="/movies" />
-        </Route>
-      </Switch>
-    </Main>
+    <Switch>
+      <Route path="/movies">
+        <MovieList />
+      </Route>
+      <Route path="/shows"></Route>
+      <Route path="/people"></Route>
+      <Route path="/">
+        <Redirect to="/movies" />
+      </Route>
+    </Switch>
   </HashRouter>
 );
 
--- src/features/List/Movies/index.tsx
+++ src/features/List/Movies/index.tsx
@@ -3,25 +3,30 @@ import { Header, List } from "./styled";
 import { movies } from "../../../common/movies";
 import ListTile from "../../../common/Tiles/List";
 import { MovieProps } from "../../../types/movieprops";
+import Main from "../../../common/main";
+import Pagination from "../../Pagination";
 
 const MovieList = () => {
   return (
     <>
-      <Header>Popular Movies</Header>
-      <List>
-        {movies.map(
-          ({ title, year, poster, rating, votes, genres }: MovieProps) => (
-            <ListTile
-              title={title}
-              year={year}
-              poster={poster}
-              rating={rating}
-              votes={votes}
-              genres={genres}
-            />
-          )
-        )}
-      </List>
+      <Main>
+        <Header>Popular Movies</Header>
+        <List>
+          {movies.map(
+            ({ title, year, poster, rating, votes, genres }: MovieProps) => (
+              <ListTile
+                title={title}
+                year={year}
+                poster={poster}
+                rating={rating}
+                votes={votes}
+                genres={genres}
+              />
+            )
+          )}
+        </List>
+      </Main>
+      <Pagination />
     </>
   );
 };

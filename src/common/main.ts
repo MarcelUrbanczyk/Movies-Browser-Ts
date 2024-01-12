9141cf1cc616fc0f3e260415ab9101c27393b81e
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-01-12 22:54:23 +0100

    added padding

--- src/common/main.ts
+++ src/common/main.ts
@@ -1,8 +1,9 @@
 import { styled } from "styled-components";
 
 const Main = styled.main`
-  max-width: 1368px;
+  max-width: 1448px;
   margin: 56px auto 40px auto;
+  padding: 0 40px;
 `;
 
 export default Main;

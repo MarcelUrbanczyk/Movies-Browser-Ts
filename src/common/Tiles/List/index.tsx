f3a97d6bf11d289f00ab76a97b9bd6c6645a3e2d
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-01-12 20:49:54 +0100

    created list tile component

--- src/common/Tiles/List/index.tsx
+++ src/common/Tiles/List/index.tsx
@@ -1,3 +1,57 @@
 import React from "react";
-const ListTile = ({ title, subtitle, icon, onClick }) => <></>;
+import { MovieProps } from "../../../types/movieprops";
+import {
+  Poster,
+  Tile,
+  Frame,
+  Title,
+  Year,
+  Rating,
+  IconWrapper,
+  Score,
+  Votes,
+  GenreList,
+  Genre,
+} from "./styled";
+import Star from "../starIcon";
+
+const ListTile = ({
+  title,
+  year,
+  poster,
+  rating,
+  votes,
+  genres,
+}: MovieProps) => (
+  <Tile>
+    <Frame>
+      <Poster
+        src={`https://image.tmdb.org/t/p/original/${poster}`}
+        alt={title}
+      />
+    </Frame>
+    <Title>{title}</Title>
+    {year && <Year>{year}</Year>}
+    {genres && (
+      <GenreList>
+        {genres.map((genre) => (
+          <Genre>{genre}</Genre>
+        ))}
+      </GenreList>
+    )}
+    <Rating>
+      {rating && votes ? (
+        <>
+          <IconWrapper>
+            <Star />
+          </IconWrapper>
+          <Score>{rating}</Score>
+          <Votes>{votes} &nbsp;votes</Votes>
+        </>
+      ) : (
+        <Votes>No votes yet</Votes>
+      )}
+    </Rating>
+  </Tile>
+);
 export default ListTile;

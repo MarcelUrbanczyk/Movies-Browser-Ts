7e6162484a6a7a37d295470b40525ad0c103b19a
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-01-13 15:30:38 +0100

    replaced star icon svg with png

---
+++ src/common/Tiles/starIcon.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Star = styled.img`
+  width: 20px;
+  height: 20px;
+  @media (max-width: 560px) {
+    width: 16px;
+    height: 16px;
+  }
+`;

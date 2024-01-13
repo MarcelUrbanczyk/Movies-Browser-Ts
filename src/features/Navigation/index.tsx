92c29b547357de1f743061183dcfa79713bbfd18
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-01-13 16:11:18 +0100

    links now redirect to different pages

--- src/features/Navigation/index.tsx
+++ src/features/Navigation/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import {
   Logo,
   Name,
-  NavLink,
+  StyledNavLink,
   NavLinks,
   SearchBar,
   StyledNavigation,
@@ -11,25 +11,31 @@ import {
 } from "./styled";
 import Icon from "./icon";
 import Loupe from "./loupe";
+import { useLocation } from "react-router";
+import { toHome, toMovies, toPeople, toShows } from "../../core/routes";
 
-const Navigation = () => (
-  <StyledNavigation>
-    <Wrapper>
-      <Logo>
-        <Icon />
-        <Name>Movies Browser</Name>
-      </Logo>
-      <NavLinks>
-        <NavLink>Movies</NavLink>
-        <NavLink>Shows</NavLink>
-        <NavLink>People</NavLink>
-      </NavLinks>
-      <SearchBar>
-        <Loupe />
-        <Input placeholder="Search for movies..." />
-      </SearchBar>
-    </Wrapper>
-  </StyledNavigation>
-);
+const Navigation = () => {
+  const location = useLocation();
+  console.log(location.pathname.startsWith("/movies"));
+  return (
+    <StyledNavigation>
+      <Wrapper>
+        <Logo to={toHome}>
+          <Icon />
+          <Name>Movies Browser</Name>
+        </Logo>
+        <NavLinks>
+          <StyledNavLink to={toMovies}>Movies</StyledNavLink>
+          <StyledNavLink to={toShows}>Shows</StyledNavLink>
+          <StyledNavLink to={toPeople}>People</StyledNavLink>
+        </NavLinks>
+        <SearchBar>
+          <Loupe />
+          <Input placeholder="Search for movies..." />
+        </SearchBar>
+      </Wrapper>
+    </StyledNavigation>
+  );
+};
 
 export default Navigation;

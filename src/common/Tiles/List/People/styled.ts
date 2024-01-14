641e0c5f02b71c4002859b8b1627b228acf03a47
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-01-14 17:10:06 +0100

    made people list tile responsive

--- src/common/Tiles/List/People/styled.ts
+++ src/common/Tiles/List/People/styled.ts
@@ -2,12 +2,21 @@ import { styled } from "styled-components";
 
 export const Tile = styled.article`
   padding: 16px;
+  max-width: 208px;
   background-color: ${({ theme }) => theme.box};
   border-radius: ${({ theme }) => theme.borderRadius};
   display: flex;
   flex-direction: column;
   gap: 12px;
   box-shadow: ${({ theme }) => theme.shadow};
+  @media (max-width: 1220px) {
+    max-width: 164px;
+  }
+  @media (max-width: 560px) {
+    max-width: 136px;
+    gap: 8px;
+    padding: 8px;
+  }
 `;
 
 export const Frame = styled.span`
@@ -15,6 +24,14 @@ export const Frame = styled.span`
   height: 231px;
   border-radius: ${({ theme }) => theme.borderRadius};
   overflow: hidden;
+  @media (max-width: 1220px) {
+    width: 132px;
+    height: 173px;
+  }
+  @media (max-width: 560px) {
+    width: 120px;
+    height: 178px;
+  }
 `;
 
 export const Name = styled.p`
@@ -23,6 +40,12 @@ export const Name = styled.p`
   font-size: 22px;
   font-weight: 500;
   line-height: 130%;
+  @media (max-width: 1220px) {
+    font-size: 18px;
+  }
+  @media (max-width: 560px) {
+    font-size: 14px;
+  }
 `;
 
 export const Image = styled.img`

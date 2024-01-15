6772f3c7968344716ec4a1f265a27b5498c95cdf
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-01-15 21:47:44 +0100

    separated types for tile and movie props

--- src/types/movieprops.ts
+++ src/types/movieprops.ts
@@ -1,8 +1,17 @@
 export type MovieProps = {
   title: string;
+  release_date: string;
+  poster_path?: string;
+  vote_average: number;
+  vote_count: number;
+  genre_ids: number[];
+};
+
+export type MovieListTileProps = {
+  title: string;
   year?: string;
   poster?: string;
-  rating?: string;
-  votes?: string;
-  genres?: string[];
+  rating?: number;
+  votes?: number;
+  genres?: number[];
 };

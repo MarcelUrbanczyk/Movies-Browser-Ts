e6e44e87328c87ed18e2dcd242fdd52a0bdbec52
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-01-17 21:53:40 +0100

    impoved responsivity of error

--- src/features/Error/styled.ts
+++ src/features/Error/styled.ts
@@ -5,14 +5,25 @@ export const Wrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   text-align: center;
-
   margin-top: 150px;
   gap: 24px;
+  @media (max-width: 680px) {
+    margin-top: 100px;
+  }
+  @media (max-width: 560px) {
+    margin-top: 60px;
+  }
 `;
 
 export const ErrorIcon = styled.img`
   max-width: 120px;
   margin: 0 auto;
+  @media (max-width: 680px) {
+    max-width: 80px;
+  }
+  @media (max-width: 560px) {
+    max-width: 60px;
+  }
 `;
 
 export const ErrorHeader = styled.h2`
@@ -20,6 +31,12 @@ export const ErrorHeader = styled.h2`
   font-size: 36px;
   font-weight: 600;
   line-height: 120%;
+  @media (max-width: 680px) {
+    font-size: 30px;
+  }
+  @media (max-width: 560px) {
+    font-size: 20px;
+  }
 `;
 
 export const ErrorMesssage = styled.p`
@@ -27,6 +44,12 @@ export const ErrorMesssage = styled.p`
   font-size: 22px;
   font-weight: 500;
   line-height: 130%;
+  @media (max-width: 680px) {
+    font-size: 18px;
+  }
+  @media (max-width: 560px) {
+    font-size: 12px;
+  }
 `;
 
 export const Button = styled.button`
@@ -43,5 +66,14 @@ export const Button = styled.button`
   cursor: pointer;
   &:hover {
     background-color: #003399;
+  }
+  @media (max-width: 680px) {
+    padding: 12px 16px;
+    font-size: 12px;
+    font-weight: 500;
+  }
+  @media (max-width: 560px) {
+    padding: 8px 12px;
+    font-size: 10px;
   }
 `;

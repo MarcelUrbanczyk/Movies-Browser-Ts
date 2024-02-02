ebcb1898cc82e0488594a2ea3a9ba383a11d3d7c
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-02-02 22:52:15 +0100

    added searching movies

--- src/features/List/Movies/index.tsx
+++ src/features/List/Movies/index.tsx
@@ -3,11 +3,16 @@ import Success from "./Success";
 import Error from "../../Error";
 import Loading from "../../Loading";
 import { getMovies } from "../../getData";
+import { useLocation } from "react-router";
 
 const MovieList = () => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const query = searchParams.get("search");
+
   const { isLoading, error, data } = useQuery({
     queryKey: ["movies"],
-    queryFn: getMovies,
+    queryFn: () => getMovies(query, 1),
   });
 
   if (isLoading) return <Loading />;

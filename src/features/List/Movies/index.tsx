9f6139b834af234a010c1f0af5a02d552603a1f8
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-02-03 12:03:37 +0100

    added debounce to use query

--- src/features/List/Movies/index.tsx
+++ src/features/List/Movies/index.tsx
@@ -4,14 +4,15 @@ import Error from "../../Error";
 import Loading from "../../Loading";
 import { getMovies } from "../../getData";
 import { useLocation } from "react-router";
+import { useDebounce } from "../../useDebounce";
 
 const MovieList = () => {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
-  const query = searchParams.get("search");
+  const query = useDebounce(searchParams.get("search"), 1 * 1000);
 
   const { isLoading, error, data } = useQuery({
-    queryKey: ["movies"],
+    queryKey: ["movies", query],
     queryFn: () => getMovies(query, 1),
   });
 

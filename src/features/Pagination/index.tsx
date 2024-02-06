c29ae12952b715ba157ad10734df9583da0e84f9
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-02-06 16:01:05 +0100

    created switching pages

--- src/features/Pagination/index.tsx
+++ src/features/Pagination/index.tsx
@@ -8,11 +8,41 @@ import {
   MobileArrowWrapper,
 } from "./styled";
 import { useState, useEffect } from "react";
+import { useLocation, useHistory } from "react-router";
 
 const Pagination = ({ totalPages }) => {
-  const page: number = 1;
+  const history = useHistory();
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const pageParams = parseInt(params.get("page") || "1");
 
+  const [page, setPage] = useState(pageParams);
   const [maxPage, setMaxPage] = useState(500);
+
+  const onPageChange = (action) => {
+    let newPage;
+
+    switch (action) {
+      case "first":
+        newPage = 1;
+        break;
+      case "previous":
+        newPage = page - 1;
+        break;
+      case "next":
+        newPage = page + 1;
+        break;
+      case "last":
+        newPage = maxPage;
+        break;
+      default:
+        newPage = 1;
+        break;
+    }
+
+    history.push({ search: `?page=${newPage}` });
+    setPage(newPage);
+  };
 
   useEffect(() => {
     if (totalPages < 500) {
@@ -22,25 +52,25 @@ const Pagination = ({ totalPages }) => {
 
   return (
     <Wrapper>
-      <Button disabled={page === 1}>
+      <Button onClick={() => onPageChange("first")} disabled={page === 1}>
         <Arrow $disabled={page === 1} />
         <MobileArrowWrapper>
           <Arrow $disabled={page === 1} />
         </MobileArrowWrapper>
         <Paragraph>First</Paragraph>
       </Button>
-      <Button disabled={page === 1}>
+      <Button onClick={() => onPageChange("previous")} disabled={page === 1}>
         <Arrow $disabled={page === 1} />
         <Paragraph>Previous</Paragraph>
       </Button>
       <Pages>
-        Page <Strong>1</Strong> of <Strong>{maxPage}</Strong>
+        Page <Strong>{page}</Strong> of <Strong>{maxPage}</Strong>
       </Pages>
-      <Button disabled={page === maxPage}>
+      <Button onClick={() => onPageChange("next")} disabled={page === maxPage}>
         <Paragraph>Next</Paragraph>
         <Arrow $flipped $disabled={page === maxPage} />
       </Button>
-      <Button disabled={page === maxPage}>
+      <Button onClick={() => onPageChange("last")} disabled={page === maxPage}>
         <Paragraph>Last</Paragraph>
         <Arrow $flipped $disabled={page === maxPage} />
         <MobileArrowWrapper>

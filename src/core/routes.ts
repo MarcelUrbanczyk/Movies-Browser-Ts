8804ad1c90bb5403a015793a1477415305c10aa0
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-02-11 15:28:34 +0100

    fixed routes

--- src/core/routes.ts
+++ src/core/routes.ts
@@ -1,7 +1,7 @@
 export const toMovies = "/movies";
-export const toMovie = (id) => `/movies/${id}`;
+export const toMovie = `/movies/:id`;
 export const toPeople = "/people";
-export const toPerson = (id) => `/people/${id}`;
+export const toPerson = `/people/:id`;
 export const toShows = "/shows";
-export const toShow = (id) => `/shows/${id}`;
+export const toShow = `/shows/:id`;
 export const toHome = "/";

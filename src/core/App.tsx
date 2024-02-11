9d002651ad0024826b60a114ca9a4b06d4776004
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-02-11 15:28:28 +0100

    added movie page to app

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -3,22 +3,27 @@ import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import MovieList from "../features/List/Movies";
 import PeopleList from "../features/List/People";
 import ShowList from "../features/List/Shows";
+import { toHome, toMovies, toPeople, toShows, toMovie } from "./routes";
+import MoviePage from "../features/Page/Movie";
 
 const App = () => (
   <HashRouter>
     <Navigation />
     <Switch>
-      <Route path="/movies">
+      <Route path={toMovie}>
+        <MoviePage />
+      </Route>
+      <Route path={toMovies}>
         <MovieList />
       </Route>
-      <Route path="/shows">
+      <Route path={toShows}>
         <ShowList />
       </Route>
-      <Route path="/people">
+      <Route path={toPeople}>
         <PeopleList />
       </Route>
-      <Route path="/">
-        <Redirect to="/movies" />
+      <Route path={toHome}>
+        <Redirect to={toMovies} />
       </Route>
     </Switch>
   </HashRouter>

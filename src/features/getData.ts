c02cf0e7fa491ce1cffeeb07a88ab1fb9bcf970a
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-02-11 15:43:22 +0100

    combined function to make code shorter

--- src/features/getData.ts
+++ src/features/getData.ts
@@ -1,8 +1,12 @@
 import { options } from "./queryOptions";
 
-export const getMovieGenres = async () => {
+type dataType = "movie" | "tv" | "person";
+
+type genreType = "movie" | "tv";
+
+export const getGenres = async (genreType: genreType) => {
   const response = await fetch(
-    "https://api.themoviedb.org/3/genre/movie/list",
+    `https://api.themoviedb.org/3/genre/${genreType}}/list`,
     options
   );
   const { genres } = await response.json();
@@ -10,60 +14,21 @@ export const getMovieGenres = async () => {
   return genres;
 };
 
-export const getShowGenres = async () => {
-  const response = await fetch(
-    "https://api.themoviedb.org/3/genre/tv/list",
-    options
-  );
-
-  const { genres } = await response.json();
-
-  return genres;
-};
-
-export const getMovies = async (
+export const getListData = async (
+  dataType: dataType,
   query: string | null | undefined,
   page: number
 ) => {
-  const urlPopular = `https://api.themoviedb.org/3/movie/popular${
+  const urlPopular = `https://api.themoviedb.org/3/${dataType}/popular${
     page ? `?page=${page}` : ""
   }`;
-
-  const urlSearch = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=${
-    page ? page : 1
-  }'`;
+  const urlSearch = `https://api.themoviedb.org/3/search/${dataType}?query=${query}&include_adult=false&language=en-US&page=${page}`;
 
   const response = await fetch(query ? urlSearch : urlPopular, options);
   return await response.json();
 };
 
-export const getPeople = async (
-  query: string | null | undefined,
-  page: number
-) => {
-  const urlPopular = `https://api.themoviedb.org/3/person/popular${
-    page ? `?page=${page}` : ""
-  }`;
-  const urlSearch = `https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=en-US&page=${page}`;
-
-  const response = await fetch(query ? urlSearch : urlPopular, options);
-  return await response.json();
-};
-
-export const getShows = async (
-  query: string | null | undefined,
-  page: number
-) => {
-  const urlPopular = `https://api.themoviedb.org/3/tv/popular${
-    page ? `?page=${page}` : ""
-  }`;
-  const urlSearch = `https://api.themoviedb.org/3/search/tv?query=${query}&include_adult=false&language=en-US&page=${page}`;
-
-  const response = await fetch(query ? urlSearch : urlPopular, options);
-  return await response.json();
-};
-
-export const getPageData = async (dataType: string, id: number) => {
+export const getPageData = async (dataType: dataType, id: number) => {
   const url = `https://api.themoviedb.org/3/${dataType}/${id}`;
 
   const response = await fetch(url, options);

ccd8c0074f6253b60427c70155874d6ed7f62035
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-02-11 15:29:04 +0100

    created moviePage component

--- src/features/Page/Movie/index.tsx
+++ src/features/Page/Movie/index.tsx
@@ -1,9 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import Error from "../../Error";
+import Loading from "../../Loading";
+import Success from "./Success";
+import { useLocation } from "react-router";
+import { getPageData } from "../../getData";
+
 const MoviePage = () => {
-  return (
-    <div>
-      <h1>Movie Page</h1>
-    </div>
-  );
+  const location = useLocation();
+  const id = parseInt(location.pathname.split("/")[2]);
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["movie", id],
+    queryFn: () => getPageData("movie", id),
+  });
+
+  if (isLoading) return <Loading />;
+  if (error) return <Error />;
+
+  return <Success movie={data} />;
 };
 
 export default MoviePage;

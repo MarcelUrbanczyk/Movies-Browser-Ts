e80d160cdcb6430a6dbc6dedf4f9acd9989e40cc
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-02-15 19:44:35 +0100

    added page star component

--- src/common/Tiles/starIcon.ts
+++ src/common/Tiles/starIcon.ts
@@ -1,10 +1,15 @@
 import styled from "styled-components";
 
-export const Star = styled.img`
+export const ListStar = styled.img`
   width: 20px;
   height: 20px;
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     width: 16px;
     height: 16px;
   }
+`;
+
+export const PageStar = styled.img`
+  width: 24px;
+  height: 24px;
 `;

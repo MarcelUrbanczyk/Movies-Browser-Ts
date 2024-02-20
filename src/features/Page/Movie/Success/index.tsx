5b59659e253565d32dd38e6813fd1ac65f13bee0
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-02-20 18:11:14 +0100

    added cast to movie page

--- src/features/Page/Movie/Success/index.tsx
+++ src/features/Page/Movie/Success/index.tsx
@@ -1,7 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
 import ScreenplayPageTile from "../../../../common/Tiles/Page/Screenplay";
 import Main from "../../../../common/main";
+import { getCredits } from "../../../getData";
+import { PeopleProps } from "../../../types";
+import { Header, Wrapper } from "./styled";
+import PeopleListTile from "../../../../common/Tiles/List/People";
 
 const Success = ({ movie }) => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["credits", movie.id],
+    queryFn: () => getCredits("movie", movie.id),
+  });
+
+  const cast = data?.cast || [];
+  const crew = data?.crew || [];
+
   return (
     <Main>
       <ScreenplayPageTile
@@ -14,6 +27,24 @@ const Success = ({ movie }) => {
         score={movie.vote_average}
         votes={movie.vote_count}
       />
+
+      {cast ? (
+        <>
+          <Header>Cast</Header>
+          <Wrapper>
+            {cast.map(
+              ({ name, profile_path: image, id, character }: PeopleProps) => (
+                <PeopleListTile
+                  name={name}
+                  image={image}
+                  id={id}
+                  role={character}
+                />
+              )
+            )}
+          </Wrapper>
+        </>
+      ) : null}
     </Main>
   );
 };
--- src/features/Page/Movie/Success/styled.ts
+++ src/features/Page/Movie/Success/styled.ts
@@ -1,10 +1,23 @@
 import { styled } from "styled-components";
 
+export const Header = styled.h2`
+  color: ${({ theme }) => theme.textPrimary};
+  font-size: 36px;
+  font-weight: 600;
+  line-height: 120%;
+  margin-top: 24px;
+  @media (max-width: 1446px) {
+    font-size: 30px;
+  }
+  @media (max-width: 600px) {
+    font-size: 24px;
+  }
+`;
+
 export const Wrapper = styled.div`
-  display: grid;
+  display: flex;
+  flex-wrap: wrap;
   gap: 24px;
-  grid-template-columns: repeat(auto-fit, 1fr);
-  justify-content: center;
   margin: 24px 0;
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     gap: 12px;

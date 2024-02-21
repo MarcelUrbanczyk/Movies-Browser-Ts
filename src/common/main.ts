9eca534723ba9d77d0d1394dd578409cecae28f4
Marcel Urbanczyk <urbanczyk.marcel04@gmail.com>
2024-02-21 20:46:05 +0100

    added different paddings in main component media queries

--- src/common/main.ts
+++ src/common/main.ts
@@ -4,6 +4,12 @@ const Main = styled.main`
   max-width: 1448px;
   margin: 56px auto 40px auto;
   padding: 0 40px;
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    padding: 0 24px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    padding: 0 12px;
+  }
 `;
 
 export default Main;
